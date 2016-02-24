a8cd991a9239e27259631d377ac33fadcdfd02a4
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-24 15:47:33 -0500

    created Tree model and ran migrations

--- trees_app/admin.py
+++ trees_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from trees_app.models import Tree
+
+admin.site.register(Tree)
--- trees_app/models.py
+++ trees_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Tree(models.Model):
+    name = models.CharField(max_length=20)
+    scientific_name = models.CharField(max_length=40)
+    wood = models.CharField(max_length=10, choices=[('hard', 'Hardwood'), ('soft', 'Softwood')])
+    type_tree = models.CharField(max_length=10, choices=[('dec', 'Deciduous'), ('con', 'Coniferous')])
+    max_height = models.IntegerField()
+    location = models.CharField(max_length=15)
+    uses = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
